55256c111a2a911023f566e0e999184473d71e20
FiveHeisenberg <reynzimp@gmail.com>
2024-08-03 16:34:44 +0700

    Third Commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,29 @@
 let login = document.getElementById('login');
 let key = localStorage.getItem("user");
+let navAcc = document.getElementsByClassName('nav-acc');
+let accCheck = 0;
 
 if (localStorage.getItem('user') === "admin" || key) {
     login.style.display = "none";
     logout.style.display = "block";
+    accCheck = 1
 } else {
     login.style.display = "block";
 }
 
+function userAcc() {
+    if (accCheck === 1) {
+        logOut();
+    } else if (accCheck === 0) {
+        logIn();
+    }
+}
+
+function logIn() {
+    window.location.href = 'login.html';
+}
+
 function logOut() {
     localStorage.clear();
-    window.location.href = 'login.html';
+    location.reload();
 }
